8104231b760c06fdf65a3233e15dbd56301059ee
matthewguillory65 <matthew.guillor@ad.aie.edu>
2017-01-19 17:59:09 -0600

    Practice on States

---
+++ CSharp/States/States/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Lights
+    {
+        public enum LightsState
+        {
+            INIT = 0,
+            RED = 1,
+            GREEN = 2,
+            YELLOW = 3,
+            END = 9000,
+        }
+
+        public void Red()
+        {
+
+        }
+
+        public void Green()
+        {
+
+        }
+
+        public void Yellow()
+        {
+            
+        }
+
+        public void Lightchange()
+        {
+            bool Power = true;
+            if(Power == true)
+            {
+                Stopwatch.StartNew(); 
+            }
+        }
+    }
+}

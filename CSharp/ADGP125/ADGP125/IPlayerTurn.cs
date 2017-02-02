a4481b78de78b672052adcac79655728879113b1
matthewguillory65 <matthew.guillor@ad.aie.edu>
2017-02-02 17:11:16 -0600

    Start to Assessment number 4
    Start to Assessment number 4

---
+++ CSharp/ADGP125/ADGP125/IPlayerTurn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGP125
+{
+    public interface IPlayerTurn
+    {
+        void UpdatePlayerState();
+
+        void MoveSelection();
+
+        void BattleInitiation();
+
+        void PlayerAttack();
+
+        void OpponentsAttack();
+
+        void SwitchTurn();
+
+        void SwitchPokemon();
+    }
+}
---
+++ CSharp/ADGP125/ADGP125/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADGP125
+{
+    class Program : IPlayerTurn
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void BattleInitiation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveSelection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpponentsAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayerAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SwitchPokemon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SwitchTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePlayerState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
